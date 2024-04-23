c242a95eb95dbd9dd23332883f213c698c54e508
jwathika <wathika02@gmail.com>
2024-04-24 00:08:39 +0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+
+
 const App = () => {
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="bg-purple-800 border-indigo-500">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6"></div>
+        </nav>
     );
 };
 
