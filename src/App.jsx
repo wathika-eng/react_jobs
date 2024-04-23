5de07c215f86df3356dc9f0df9a7f4e1c73a6542
jwathika <wathika02@gmail.com>
2024-04-23 23:36:31 +0300

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 const App = () => {
+  const x = 10;
+  const y = 2;
   return (
-    <div>App</div>
+    <>
+      <div style={{ color: 'red' }}> Sum is {x} + {y}</div>
+      <p className="text-5xl">Hey</p>
+    </>
   );
 };
 
